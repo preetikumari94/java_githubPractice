f05019a88bad99ad64c4dc5ff7773fa25e93911f
preetikumari94 <pree0717@gmail.com>
2024-07-15 11:05:16 +0530

    First commit

---
+++ java_githubPractice/src/java_githubPractice_test/HelloClass.java
@@ -0,0 +1,14 @@
+package java_githubPractice_test;
+public class HelloClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("welcome1");
+		System.out.println("welcome2");
+		System.out.println("welcome3");
+		System.out.println("welcome4");
+		
+
+	}
+
+}
